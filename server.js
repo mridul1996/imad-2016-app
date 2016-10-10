caf47309c34e6fab982550b609f0c8250e42d3ae
mridul1996 <mintuvyas1996@gmail.com>
2016-10-10 12:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/article-one',function(req,res){
     res.send('article-one is requested and will be served here');
 });
 
+app.get('/article-two',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
